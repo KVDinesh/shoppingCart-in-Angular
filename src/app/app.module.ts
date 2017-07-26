9ea6b6fb6867a47da20a8abe0fa59f791e1d0bd4
dinesh <dineshrockz66@gmail.com>
2017-07-26 17:50:20 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'signup',component:SignupComponent},
+      {path:'home',component:HomeComponent}
+    ])  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from "./product.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+   title = 'app';
+  public cart = [];
+  constructor(private productService:ProductService) { 
+  }
+   products = [];
+  ngOnInit() {
+    this.productService.getProducts().subscribe(responseProducts => this.products = responseProducts);
+  }
+  addtocart(name)
+  {
+      this.cart.push(name);
+  }
+}
