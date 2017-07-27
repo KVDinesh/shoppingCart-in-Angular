145bffe6f123e2772349bc1b205ee68b570bffc1
dinesh <dineshrockz66@gmail.com>
2017-07-27 17:21:30 -0500

    second commit :wq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SigninComponent } from './signin/signin.component';
 import { HomeComponent } from './home/home.component';
 import { ProductService } from "./home/product.service";
 import { HttpModule } from "@angular/http/";
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -15,13 +16,15 @@ import { HttpModule } from "@angular/http/";
     SignupComponent,
     SigninComponent,
     HomeComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot([
       {path:'signup',component:SignupComponent},
-      {path:'home',component:HomeComponent}
+      {path:'home',component:HomeComponent},
+      {path:'cart',component:CartComponent}
     ])  
   ],
   providers: [ProductService],
